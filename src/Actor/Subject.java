3888039532d714381256601bc449224d7f78466b
raakeshUTM <128468074+raakeshUTM@users.noreply.github.com>
2024-06-18 20:03:26 +0800

    r

--- src/Actor/Subject.java
+++ src/Actor/Subject.java
@@ -16,6 +16,7 @@ public class Subject {
     private String code;
     private String name;
     private int creditHour;
+    private int flag;
 
     // private ArrayList<Student>registeredStudents;
     // private Lecturer lecturer;
@@ -23,10 +24,11 @@ public class Subject {
 
     // Constructor, getters, setters, etc.
 
-    public Subject(String code, String name, int creditHour) {
+    public Subject(String code, String name, int creditHour, int flag) {
         this.code = code;
         this.name = name;
         this.creditHour = creditHour;
+        this.flag = flag;
     }
 
     public String getCode() {
@@ -41,16 +43,21 @@ public class Subject {
         return creditHour;
     }
 
+    public int getFlag() {
+        return flag;
+    }
+
     public void readFile(ArrayList<Subject> subjectList) throws IOException {
         Scanner inpFile = new Scanner(new File("subjectList.csv"));
         inpFile.useDelimiter(",|\\n");
         while (inpFile.hasNext()) {
             String code = inpFile.next();
             int creditHour = inpFile.nextInt();
+            int flag = inpFile.nextInt();
             String name = inpFile.nextLine();
             name = name.substring(1);
 
-            Subject subject = new Subject(code, name, creditHour);
+            Subject subject = new Subject(code, name, creditHour, flag);
             subjectList.add(subject);
         }
         inpFile.close();
